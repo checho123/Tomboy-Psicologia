8dbbdde51e375981e2d4b1dfa461148fa132ad22
Sergio Garcia <checho12g@gmail.com>
2025-09-06 19:48:54 -0500

    Se añadio mejora para el inventario de fruitas y su manera de poder agregar creditos

--- Assets/Scripts/Credits/FruitInventory.cs
+++ Assets/Scripts/Credits/FruitInventory.cs
@@ -1,16 +1,32 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class FruitInventory : MonoBehaviour
+public class FruitInventory : SingletonManager<FruitInventory>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    FruitInventory()
     {
-        
+        EnablePersistence = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Settings Fruits")]
+    [SerializeField] private List<FruitSettings> fruitSettings = new();
+}
+
+[Serializable]
+public class FruitSettings
+{
+    public string nameFruit;
+    public int valueFruit;
+    public FruitType fruitType;
+}
+
+public enum FruitType
+{
+    none = 0,
+    Apple = 1,
+    Banana = 2,
+    Cherries = 3,
+    Melon = 4,
+    Pineapple = 5,
 }
--- Assets/Scripts/Credits/FruitPickup.cs
+++ Assets/Scripts/Credits/FruitPickup.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class FruitPickup
+public class FruitPickup : MonoBehaviour
 {
-   
+    [Header("Fruit Type")]
+    [SerializeField] private FruitType type;
 }
