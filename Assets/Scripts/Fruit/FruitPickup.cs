ac69b95255c4aedcf05dd9249ac83d5f88cfedba
Kevin Padilla <kevinpadillagomez70@gmail.com>
2025-09-08 01:38:01 -0500

    se realizo la creacion del mapa 100 se agrotambien enemigos visuales , se modificaron los prefasb, y se se creo el script que desteuye las fruits

--- Assets/Scripts/Fruit/FruitPickup.cs
+++ Assets/Scripts/Fruit/FruitPickup.cs
@@ -4,4 +4,24 @@ public class FruitPickup : MonoBehaviour
 {
     [Header("Fruit Type")]
     [SerializeField] private FruitType type;
+    public string triggerName = "Collision";
+    public float destroyAfter = 0.6f;
+
+    private Animator anim;
+    
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log($"El Player tocó la fruta {type}");
+            anim.SetTrigger(triggerName);
+            Destroy(gameObject, destroyAfter);
+        }
+    }
 }
